fcfe638f2ae840a499536bb2bfa39bc5ec27d2c4
Qingwei Lan <qingweilan@gmail.com>
2016-05-17 23:32:51 -0700

    Added argument recognition

--- src/lxc/lxc_export.c
+++ src/lxc/lxc_export.c
@@ -23,8 +23,58 @@
 
 #include <stdio.h>
 
+#include <lxc/lxccontainer.h>
+
+#include "arguments.h"
+
+static char *export_name = NULL;
+
+static int my_parser(struct lxc_arguments* args, int c, char* arg)
+{
+	switch (c) {
+	case 'e': export_name = arg; break;
+	}
+	return 0;
+}
+
+static const struct option my_longopts[] = {
+	{"export", required_argument, 0, 'e'},
+	LXC_COMMON_OPTIONS,
+};
+
+static struct lxc_arguments my_args = {
+	.progname = "lxc-export",
+	.help     = "\
+--name=NAME\n\
+\n\
+lxc-export exports a container or snapshot\n\
+\n\
+Options :\n\
+  -n, --name=NAME       NAME of the container\n\
+  -e, --export=NAME     NAME of the output container\n",
+	.name     = NULL,
+	.options  = my_longopts,
+	.parser   = my_parser,
+	.checker  = NULL,
+};
+
 int main(int argc, char *argv[])
 {
-	printf("Within lxc-export\n");
-	return 0;
+	int ret = EXIT_FAILURE;
+
+	if (lxc_arguments_parse(&my_args, argc, argv))
+		goto out;
+
+	if (!my_args.log_file)
+		my_args.log_file = "none";
+
+	if (!export_name) {
+		printf("%s: missing output name, use --export option\n", my_args.progname);
+		goto out;
+	}
+
+	printf("name: %s, export name: %s\n", my_args.name, export_name);
+
+out:
+	return ret;
 }
